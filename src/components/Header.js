eba16c28388cad8abe3114669609ecd4ae144d1a
Ali Haydar <ali.haydar85@gmail.com>
2019-11-02 10:35:32 +1300

    Cleanup files. Build first version of the header and style it

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,27 @@
 import React from 'react';
-import Logo from '../assets/images/ali-logo.png';
+import AliLogo from '../assets/icons/AliLogo';
+import GithubIcon from '../assets/icons/GithubIcon';
+import EmailIcon from '../assets/icons/EmailIcon';
+import LinkedinIcon from '../assets/icons/LinkedinIcon';
+import TwitterIcon from '../assets/icons/TwitterIcon';
+import './Header.scss';
+
+const menuItems = ['home', 'work', 'blog'];
 
 const Header = () => (
   <div className="header">
-    <img src={Logo} alt="logo" />
+    <AliLogo />
     <ul>
-      <li>| home |</li>
-      <li>| work |</li>
-      <li>| blog |</li>
+      {menuItems.map(item => (
+        <li>item</li>
+      ))}
     </ul>
-    <div className="social-media-icons">Kousa</div>
+    <div className="social-media-icons">
+      <GithubIcon />
+      <TwitterIcon />
+      <LinkedinIcon />
+      <EmailIcon />
+    </div>
   </div>
 );
 
