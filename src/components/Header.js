51f27717ecdfc5592b8cab2159e7637f3bc7c587
Ali Haydar <ali.haydar85@gmail.com>
2019-12-14 19:04:16 +1300

    change social media logos and update css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,30 @@
 import React from 'react';
 import AliLogo from '../assets/icons/AliLogo';
-import GithubIcon from '../assets/icons/GithubIcon';
-import EmailIcon from '../assets/icons/EmailIcon';
-import LinkedinIcon from '../assets/icons/LinkedinIcon';
-import TwitterIcon from '../assets/icons/TwitterIcon';
+import { ReactComponent as GithubIcon } from '../assets/icons/GithubIcon.svg';
+import { ReactComponent as EmailIcon } from '../assets/icons/EmailIcon.svg';
+import { ReactComponent as LinkedinIcon } from '../assets/icons/LinkedinIcon.svg';
+import { ReactComponent as TwitterIcon } from '../assets/icons/TwitterIcon.svg';
 import './Header.scss';
 
 const menuItems = ['home', 'work', 'blog'];
 
 const Header = () => (
   <div className="header">
-    <AliLogo />
-    <ul>
-      {menuItems.map(item => (
-        <li key={item}>{item}</li>
-      ))}
-    </ul>
+    <div>
+      <AliLogo />
+    </div>
+    <div>
+      <ul>
+        {menuItems.map(item => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </div>
     <div className="social-media-icons">
-      <GithubIcon />
-      <TwitterIcon />
-      <LinkedinIcon />
-      <EmailIcon />
+      <EmailIcon className="social-media-icon" />
+      <TwitterIcon className="social-media-icon" />
+      <LinkedinIcon className="social-media-icon" />
+      <GithubIcon className="social-media-icon" />
     </div>
   </div>
 );
