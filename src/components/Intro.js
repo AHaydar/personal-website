d35f89bfcdb5cec106302a8176f145fc91bad639
Ali Haydar <ali.haydar85@gmail.com>
2019-12-29 20:02:49 +1300

    update introduction

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -4,11 +4,10 @@ import Button from './Button';
 import './intro.scss';
 
 const introTitle = `Hi, I'm Ali.`;
+
 const introData = [
-  'A full-stack Software Engineer who lives in Auckland, New Zealand. I use JavaScript in my daily job and when I code for fun - current focus is React and Node.js.',
-  'Learning new things, usually about technology, keeps me motivated. I like to stay closely in touch with the business goals, and I enjoy delivering high quality products. This involves people, code and processes.',
-  'I consider myself lucky I work in the software development industry as it aligns well with my passion to solve problems - programming languages are merely enablement tools.',
-  'Previous work experiences centered on the testing aspect of Software engineering, with involvement in architecture and cloud solutions. This has enabled me to become a better software engineer and think about products form all aspects, keeping the user perspective in mind at all times',
+  'A software engineer and lifelong learner. I strongly believe that good software is built through good collaboration and communication, and hence I am a big fan of the DevOps ways of working.',
+  'I try to find a balance between being a generalist and a specialist in software. My previous work has given me a solid experience in development, quality, cloud solutions, leadership and delivery. At the same time, I like to stay immersed in the code; I currently develop using JavaScript (React & Nodejs).',
   'In my spare time, when I am not geeking out, I cycle, cook, do yoga and play board games with friends.'
 ];
 
